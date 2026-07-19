ca1868677ab439e7f8c20561302180f99c71fc7c
HikerHarsh <hikerharsh@github.com>
2026-07-19 10:20:45 +0530

    Update text from Open Source to Currently Free

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
                           Download for Windows
                       </a>
-                      <p className="version-info">Version 1.0.0 &bull; Free & Open Source</p>
+                      <p className="version-info">Version 1.0.0 &bull; Currently Free</p>
                   </div>
               </div>
               <div className="hero-visual fade-in-up delay-1">
